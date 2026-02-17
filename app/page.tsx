ed48055941c0361531bac94ac48172f6365183e2
Dev Hayat <devhayat25@gmail.com>
2026-02-17 16:01:33 +0500

    comply with v16 gods

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,30 @@
 import ExploreBtn from "@/components/ExploreBtn";
-import { EventCard } from "@/components/EventCard";
+import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const page = async () => {
+const Page = async () => {
   "use cache";
   cacheLife("hours");
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/events`,
-  );
-
+  const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
   return (
     <section>
       <h1 className="text-center">
-        The Hub For Every Dev <br />
-        Event You Can't Miss
+        The Hub for Every Dev <br /> Event You Can't Miss
       </h1>
-
       <p className="text-center mt-5">
-        Hackathons, Meetups and Conferences, All in One Place{" "}
+        Hackathons, Meetups, and Conferences, All in One Place
       </p>
 
       <ExploreBtn />
 
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
+
         <ul className="events">
           {events &&
             events.length > 0 &&
@@ -43,4 +39,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default Page;
