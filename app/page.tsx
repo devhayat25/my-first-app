e158d3d5159390e13c82fc19007622e93728e1f3
Dev Hayat <devhayat25@gmail.com>
2026-02-18 14:00:14 +0500

    cloud db

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
   "use cache";
-  cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
